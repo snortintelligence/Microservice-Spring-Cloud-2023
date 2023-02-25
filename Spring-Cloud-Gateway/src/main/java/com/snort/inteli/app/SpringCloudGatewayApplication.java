430a857cfbb4b5dd3135cf5aeb49d640c07de610
snortintelligence <snortintelligence@gmail.com>
2023-02-25 18:58:40 +0530

    Any request comes first to the api gatewy and it matches with service-utl-pattern to route the specific service.

---
+++ Spring-Cloud-Gateway/src/main/java/com/snort/inteli/app/SpringCloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.snort.inteli.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+		System.out.println("Spring-Cloud-api-Gateway service started...");
+	}
+
+}
---
+++ Spring-Cloud-Gateway/src/test/java/com/snort/inteli/app/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snort.inteli.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
